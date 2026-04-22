ffc98be48c03984209d4c8f9bf5322eeda990504
Bhushan vs <bhushanvs06@gmail.com>
2026-04-22 11:26:35 +0530

    new files

--- main.java
+++ main.java
@@ -1,6 +1,26 @@
+class aniket{
+	int rel(int a,int b){
+		return a+b;
+	}
+	
+}
+
+class tan extends aniket{
+	int rel(int a,int b){
+		return a+b+100;
+	}
+	int life(int q,int t){
+		return super.rel(q,t);
+	}
+	
+}
+
 public class main{
+
 	public static void main(String[] args) {
 		System.out.println("hii");
 		System.out.println("tan");
+		tan t = new tan();
+		System.out.println(t.life(1,7));
 	}
 }
