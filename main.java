21a6845b632bb05b864fc4ba5d87478ad0bbfb96
Bhushan vs <bhushanvs06@gmail.com>
2026-04-22 11:56:22 +0530

    timepass

--- main.java
+++ main.java
@@ -15,11 +15,34 @@ class tan extends aniket{
 	
 }
 
+class Data{
+	String n;
+	int a;
+	Data(String name,int age){
+		this.n = name;
+		this.a = age;
+	}
+	String greet(){
+		return("hi "+n);
+	}
+	String changename(String name){
+		this.n = name;
+		return "done bro";
+	}
+}
+
 public class main{
 
 	public static void main(String[] args) {
 		System.out.println("hii");
 		System.out.println("tan");
 		//this is previouse safe version
+		Data d = new Data("Aniket",80);
+		Data d1=new Data("bhushan",100);
+		System.out.println(d.greet());
+		System.out.println(d1.greet());
+		// System.out.println(d.changename("bhushan"));
+		// System.out.println(d.greet());
+
 	}
 }
